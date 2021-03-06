1652939a256e9d9fea713e049f1e0b2e35b604a4
Okwukwe Ewurum <okwukwe@peexoo.ai>
2021-03-06 17:31:03 +0100

    feat(auth): user registration :sparkles:

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\AuthRequest;
+use App\Http\Resources\User\UsersResource;
+
+class RegisterController extends Controller
+{
+    /**
+     * Register a new user.
+     */
+    public function __invoke(AuthRequest $request): JsonResponse
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User Created Successfully',
+            'data' => new UsersResource($user)
+        ]);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,15 @@
 
 namespace App\Models;
 
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -40,4 +41,29 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = ['avatar'];
+
+    /**
+     * Generate user avatar url.
+     */
+    public function getAvatarAttribute(): string
+    {
+        $name = $this->prepareNameForAvatar();
+
+        return "https://ui-avatars.com/api/?name={$name}";
+    }
+
+    /**
+     * Replace space with + for avatar url
+     */
+    protected function prepareNameForAvatar(): string
+    {
+        return str_replace(' ', '+', $this->name);
+    }
 }
